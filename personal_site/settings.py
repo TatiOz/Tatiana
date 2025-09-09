5e25515fcdb0810a074b2c2675cd516cae0b4663
TatiOz <130924174+TatiOz@users.noreply.github.com>
2025-09-09 11:28:25 +0200

    Removed not needed parts

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-9-!z88jt43ou4*c-w%#wzz&=7=+dt(o*n%!m8c=*4$2%o92dla')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
