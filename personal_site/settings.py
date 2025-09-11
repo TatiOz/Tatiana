2ad85300eb47c70d8546336966aa1d60754c088e
TatiOz <130924174+TatiOz@users.noreply.github.com>
2025-09-11 10:28:09 +0200

    Last changes

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-9-!z88jt43ou4*c-w%#wz
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
-ALLOWED_HOSTS = ["tatiana-m3aj.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["tatiana-m3aj.onrender.com", "127.0.0.1", "localhost", 'tatiana-ozhgibesova-neuroscience.onrender.com']
 ADMIN_ALLOWED_IPS = config('ADMIN_ALLOWED_IPS', default='')
 
 # Security Settings
